b388ad77e970fd40a29c9f3acac73826fb175b08
0x0Louis <louis@joepegs.com>
2024-04-22 13:38:51 +0200

    Add moePerSecond

--- test/LBHooksLens.t.sol
+++ test/LBHooksLens.t.sol
@@ -50,7 +50,7 @@ contract LBHooksLensTest is TestHelper {
             )
         );
 
-        lbHooksLens = new LBHooksLens(address(lbHooksManager));
+        lbHooksLens = new LBHooksLens(address(lbHooksManager), address(masterchef));
 
         vm.label(address(lbHooks), "lbHooksRewarder");
         vm.label(address(lbHooksExtra), "lbHooksExtraRewarder");
@@ -84,103 +84,104 @@ contract LBHooksLensTest is TestHelper {
         );
         assertEq(rewarderData.rewardToken, address(moe), "test_GetPendingRewardSwapAndTransfer::3");
         assertEq(rewarderData.pid, 0, "test_GetPendingRewardSwapAndTransfer::4");
-        assertEq(rewarderData.activeId, DEFAULT_ID, "test_GetPendingRewardSwapAndTransfer::5");
-        assertEq(rewarderData.rangeStart, DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::6");
-        assertEq(rewarderData.rangeEnd, DEFAULT_ID + 2, "test_GetPendingRewardSwapAndTransfer::7");
+        assertEq(rewarderData.moePerSecond, 1e18, "test_GetPendingRewardSwapAndTransfer::5");
+        assertEq(rewarderData.activeId, DEFAULT_ID, "test_GetPendingRewardSwapAndTransfer::6");
+        assertEq(rewarderData.rangeStart, DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::7");
+        assertEq(rewarderData.rangeEnd, DEFAULT_ID + 2, "test_GetPendingRewardSwapAndTransfer::8");
 
         assertEq(
             uint8(extraRewarderData.hooksType),
             uint8(ILBHooksManager.LBHooksType.ExtraRewarder),
-            "test_GetPendingRewardSwapAndTransfer::8"
+            "test_GetPendingRewardSwapAndTransfer::9"
         );
         assertEq(
             Hooks.encode(extraRewarderData.hooksParameters),
             lbHooks.getExtraHooksParameters(),
-            "test_GetPendingRewardSwapAndTransfer::9"
+            "test_GetPendingRewardSwapAndTransfer::10"
         );
-        assertEq(extraRewarderData.rewardToken, address(rewardToken01), "test_GetPendingRewardSwapAndTransfer::10");
-        assertEq(extraRewarderData.rewardPerSecond, 1e18, "test_GetPendingRewardSwapAndTransfer::11");
-        assertEq(extraRewarderData.lastUpdateTimestamp, block.timestamp, "test_GetPendingRewardSwapAndTransfer::12");
-        assertEq(extraRewarderData.endTimestamp, block.timestamp + 100, "test_GetPendingRewardSwapAndTransfer::13");
-        assertEq(extraRewarderData.activeId, DEFAULT_ID, "test_GetPendingRewardSwapAndTransfer::14");
-        assertEq(extraRewarderData.rangeStart, DEFAULT_ID, "test_GetPendingRewardSwapAndTransfer::15");
-        assertEq(extraRewarderData.rangeEnd, DEFAULT_ID + 1, "test_GetPendingRewardSwapAndTransfer::16");
-        assertEq(extraRewarderData.remainingRewards, 100e18, "test_GetPendingRewardSwapAndTransfer::17");
-        assertTrue(extraRewarderData.isStarted, "test_GetPendingRewardSwapAndTransfer::18");
-        assertFalse(extraRewarderData.isEnded, "test_GetPendingRewardSwapAndTransfer::19");
+        assertEq(extraRewarderData.rewardToken, address(rewardToken01), "test_GetPendingRewardSwapAndTransfer::11");
+        assertEq(extraRewarderData.rewardPerSecond, 1e18, "test_GetPendingRewardSwapAndTransfer::12");
+        assertEq(extraRewarderData.lastUpdateTimestamp, block.timestamp, "test_GetPendingRewardSwapAndTransfer::13");
+        assertEq(extraRewarderData.endTimestamp, block.timestamp + 100, "test_GetPendingRewardSwapAndTransfer::14");
+        assertEq(extraRewarderData.activeId, DEFAULT_ID, "test_GetPendingRewardSwapAndTransfer::15");
+        assertEq(extraRewarderData.rangeStart, DEFAULT_ID, "test_GetPendingRewardSwapAndTransfer::16");
+        assertEq(extraRewarderData.rangeEnd, DEFAULT_ID + 1, "test_GetPendingRewardSwapAndTransfer::17");
+        assertEq(extraRewarderData.remainingRewards, 100e18, "test_GetPendingRewardSwapAndTransfer::18");
+        assertTrue(extraRewarderData.isStarted, "test_GetPendingRewardSwapAndTransfer::19");
+        assertFalse(extraRewarderData.isEnded, "test_GetPendingRewardSwapAndTransfer::20");
 
         _addLiquidity(pair01, alice, DEFAULT_ID, 1, 10e18, 10e18);
         _addLiquidity(pair01, bob, DEFAULT_ID, 0, 30e18, 30e18);
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), alice, ids);
 
-        assertEq(rewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::20");
-        assertEq(extraRewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::21");
+        assertEq(rewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::21");
+        assertEq(extraRewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::22");
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), bob, ids);
 
-        assertEq(rewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::22");
-        assertEq(extraRewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::23");
+        assertEq(rewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::23");
+        assertEq(extraRewarderData.pendingRewards, 0, "test_GetPendingRewardSwapAndTransfer::24");
 
         vm.warp(block.timestamp + 1);
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), alice, ids);
 
-        assertApproxEqRel(rewarderData.pendingRewards, 0.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::24");
-        assertApproxEqRel(extraRewarderData.pendingRewards, 0.25e18, 1e15, "test_GetPendingRewardSwapAndTransfer::25");
+        assertApproxEqRel(rewarderData.pendingRewards, 0.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::25");
+        assertApproxEqRel(extraRewarderData.pendingRewards, 0.25e18, 1e15, "test_GetPendingRewardSwapAndTransfer::26");
 
-        assertApproxEqRel(extraRewarderData.remainingRewards, 99e18, 1e15, "test_GetPendingRewardSwapAndTransfer::26");
+        assertApproxEqRel(extraRewarderData.remainingRewards, 99e18, 1e15, "test_GetPendingRewardSwapAndTransfer::27");
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), bob, ids);
 
-        assertApproxEqRel(rewarderData.pendingRewards, 0.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::27");
-        assertApproxEqRel(extraRewarderData.pendingRewards, 0.75e18, 1e15, "test_GetPendingRewardSwapAndTransfer::28");
+        assertApproxEqRel(rewarderData.pendingRewards, 0.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::28");
+        assertApproxEqRel(extraRewarderData.pendingRewards, 0.75e18, 1e15, "test_GetPendingRewardSwapAndTransfer::29");
 
-        assertApproxEqRel(extraRewarderData.remainingRewards, 99e18, 1e15, "test_GetPendingRewardSwapAndTransfer::29");
+        assertApproxEqRel(extraRewarderData.remainingRewards, 99e18, 1e15, "test_GetPendingRewardSwapAndTransfer::30");
 
         _swap(pair01, address(this), 21e18, 0);
 
         uint24 activeId = pair01.getActiveId();
 
-        assertEq(activeId, DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::30");
+        assertEq(activeId, DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::31");
 
         vm.warp(block.timestamp + 10);
         _swap(pair01, address(this), 1e18, 0);
 
-        assertEq(pair01.getActiveId(), DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::31");
+        assertEq(pair01.getActiveId(), DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::32");
 
-        assertEq(moe.balanceOf(address(lbHooks)), 11e18, "test_GetPendingRewardSwapAndTransfer::32");
-        assertApproxEqRel(extraRewarderData.remainingRewards, 99e18, 1e15, "test_GetPendingRewardSwapAndTransfer::33");
+        assertEq(moe.balanceOf(address(lbHooks)), 11e18, "test_GetPendingRewardSwapAndTransfer::33");
+        assertApproxEqRel(extraRewarderData.remainingRewards, 99e18, 1e15, "test_GetPendingRewardSwapAndTransfer::34");
 
-        assertEq(activeId, DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::34");
+        assertEq(activeId, DEFAULT_ID - 1, "test_GetPendingRewardSwapAndTransfer::35");
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), alice, ids);
 
-        assertEq(rewarderData.activeId, activeId, "test_GetPendingRewardSwapAndTransfer::35");
-        assertApproxEqRel(rewarderData.pendingRewards, 4.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::36");
-        assertApproxEqRel(extraRewarderData.pendingRewards, 10.25e18, 1e15, "test_GetPendingRewardSwapAndTransfer::37");
+        assertEq(rewarderData.activeId, activeId, "test_GetPendingRewardSwapAndTransfer::36");
+        assertApproxEqRel(rewarderData.pendingRewards, 4.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::37");
+        assertApproxEqRel(extraRewarderData.pendingRewards, 10.25e18, 1e15, "test_GetPendingRewardSwapAndTransfer::38");
 
-        assertApproxEqRel(extraRewarderData.remainingRewards, 89e18, 1e15, "test_GetPendingRewardSwapAndTransfer::38");
+        assertApproxEqRel(extraRewarderData.remainingRewards, 89e18, 1e15, "test_GetPendingRewardSwapAndTransfer::39");
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), bob, ids);
 
-        assertEq(rewarderData.activeId, activeId, "test_GetPendingRewardSwapAndTransfer::39");
-        assertApproxEqRel(rewarderData.pendingRewards, 6.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::40");
-        assertApproxEqRel(extraRewarderData.pendingRewards, 0.75e18, 1e15, "test_GetPendingRewardSwapAndTransfer::41");
+        assertEq(rewarderData.activeId, activeId, "test_GetPendingRewardSwapAndTransfer::40");
+        assertApproxEqRel(rewarderData.pendingRewards, 6.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::41");
+        assertApproxEqRel(extraRewarderData.pendingRewards, 0.75e18, 1e15, "test_GetPendingRewardSwapAndTransfer::42");
 
         vm.warp(block.timestamp + 90);
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), alice, ids);
 
-        assertApproxEqRel(rewarderData.pendingRewards, 40.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::42");
-        assertApproxEqRel(extraRewarderData.pendingRewards, 99.25e18, 1e15, "test_GetPendingRewardSwapAndTransfer::43");
+        assertApproxEqRel(rewarderData.pendingRewards, 40.5e18, 1e15, "test_GetPendingRewardSwapAndTransfer::43");
+        assertApproxEqRel(extraRewarderData.pendingRewards, 99.25e18, 1e15, "test_GetPendingRewardSwapAndTransfer::44");
 
-        assertApproxEqRel(extraRewarderData.remainingRewards, 0, 1e15, "test_GetPendingRewardSwapAndTransfer::44");
-        assertTrue(extraRewarderData.isEnded, "test_GetPendingRewardSwapAndTransfer::45");
+        assertApproxEqRel(extraRewarderData.remainingRewards, 0, 1e15, "test_GetPendingRewardSwapAndTransfer::45");
+        assertTrue(extraRewarderData.isEnded, "test_GetPendingRewardSwapAndTransfer::46");
 
         (rewarderData, extraRewarderData) = lbHooksLens.getHooksData(address(pair01), bob, ids);
 
-        assertApproxEqRel(rewarderData.pendingRewards, 60.475e18, 1e15, "test_GetPendingRewardSwapAndTransfer::46");
-        assertApproxEqRel(extraRewarderData.pendingRewards, 0.75e18, 1e15, "test_GetPendingRewardSwapAndTransfer::47");
+        assertApproxEqRel(rewarderData.pendingRewards, 60.475e18, 1e15, "test_GetPendingRewardSwapAndTransfer::47");
+        assertApproxEqRel(extraRewarderData.pendingRewards, 0.75e18, 1e15, "test_GetPendingRewardSwapAndTransfer::48");
     }
 }
