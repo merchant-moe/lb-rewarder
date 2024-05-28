a20420909e1224bded6af9547ba955ba0819ef19
0x0Louis <louis@joepegs.com>
2024-05-29 01:36:08 +0200

    Use Base contracts to avoid redefining the same code in multiple contracts

--- src/interfaces/ILBHooksMCRewarder.sol
+++ src/interfaces/ILBHooksMCRewarder.sol
@@ -10,16 +10,7 @@ import {ILBHooksExtraRewarder} from "./ILBHooksExtraRewarder.sol";
  * @dev Interface for the LB Hooks Rewarder
  */
 interface ILBHooksMCRewarder is ILBHooksBaseRewarder {
-    error LBHooksRewarder__InvalidLBHooksExtraRewarder();
-
-    event LBHooksExtraRewarderSet(ILBHooksExtraRewarder lbHooksExtraRewarder);
-
     function getPid() external view returns (uint256 pid);
 
     function getMasterChef() external view returns (IMasterChef masterChef);
-
-    function getExtraHooksParameters() external view returns (bytes32 extraHooksParameters);
-
-    function setLBHooksExtraRewarder(ILBHooksExtraRewarder lbHooksExtraRewarder, bytes calldata extraRewarderData)
-        external;
 }
